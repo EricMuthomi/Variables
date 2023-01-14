e444aec639c7a4fbe01b36ff0fa6ce09526c59a6
Eric Palmer <muthomiericpalmer@gmail.com>
2023-01-14 18:57:31 +0300

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
+        String name="Anonymous";
+        int age =23;
+        double desiredSalary=100000.55;
+        char gender='M';
+        boolean lookingForJob=false;
+        int num = (10 - (4 + 3)) * 6;
+        double num2=6;
+
+        System.out.println(num);
+        System.out.println(num2);
         System.out.println("Hello world!");
+        System.out.println("I'm "+name+"."+"I'm aged "+age+" and my desired salary" +
+                "as a java programmer is "+desiredSalary+ "$. My gender is "+gender+"."+
+                " Looking for a job now? "+lookingForJob);
+
+        /*String datatype store a string of characters in double quotations.
+        *int datatype stores variables in whole numbers
+        * double stores variables in decimals
+        * char stores single character variables in single quotations
+        * boolean datatype can only hold a true or false
+        * Static typing enables programmers to realise runtime errors and, therefore,
+        * write code free from bugs. */
+
+
+
     }
 }
